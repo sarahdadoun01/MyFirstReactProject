cdc45c416a02343ad28fe5694a8625ed6114c972
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-03-13 14:23:38 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Task from "./Task"
+import './App.css'
+
+function App() {
+  return (
+    <div>Task Manager
+        <h1>Task Manager</h1>
+        <Task />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/Task.jsx
@@ -0,0 +1,12 @@
+function Task(){
+    return (
+        <div>
+
+            <h2>First Task</h2>
+            <p>This is a sample task description.</p>
+
+        </div>
+    );
+}
+
+export default Task;
